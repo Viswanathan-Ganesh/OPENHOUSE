15d0372e35a3229b9b56aaa7e92f7353cca6a112
Viswanathan-Ganesh <152311052+Viswanathan-Ganesh@users.noreply.github.com>
2024-02-07 00:08:46 +0530

    Project FIles

---
+++ Assets/Scripts/CreateAndJoinRoom.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+
+public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //PhotonNetwork.LoadLevel();
+        Debug.Log("Joined");
+    }
+}
