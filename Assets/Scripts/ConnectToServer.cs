cc03e952a40fdacae4788fd159d25fadab12554e
Viswanathan-Ganesh <152311052+Viswanathan-Ganesh@users.noreply.github.com>
2024-02-06 23:20:41 +0530

    Project Files

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class ConnectToServer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
