664dd0443b247ec4589645b5b334132199183c70
Viswanathan-Ganesh <152311052+Viswanathan-Ganesh@users.noreply.github.com>
2024-02-27 22:17:03 +0530

    Project Files

--- Assets/Scripts/CreateAndJoinRoom.cs
+++ Assets/Scripts/CreateAndJoinRoom.cs
@@ -1,25 +1,73 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
+using ExitGames.Client.Photon;
+using Photon.Pun.Demo.Cockpit;
 
 public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
 {
     public TMP_InputField createInput;
     public TMP_InputField joinInput;
+    [SerializeField] private int maxPlayer;
+    private bool dummy = true;
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom(createInput.text);
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = maxPlayer;
+        PhotonNetwork.CreateRoom(createInput.text, roomOptions);
     }
 
     public void JoinRoom()
     {
         PhotonNetwork.JoinRoom(joinInput.text);
     }
+    
 
+    public void Update()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == maxPlayer && dummy)
+        {
+            //PhotonNetwork.SetMasterClient(PhotonNetwork.PlayerList[maxPlayer]);
+            CopSelector();
+            PhotonNetwork.LoadLevel("Level0");
+            dummy = false;
+        }
+    }
+    
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("Level0");
+        PhotonNetwork.LoadLevel("LobbyLevel");
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void CopSelector()
+    {
+        int rand = Random.Range(0, maxPlayer);
+        Hashtable customPropCop = new Hashtable();
+        Hashtable customPropThief = new Hashtable();
+
+        customPropCop.Add("Team", "Cop");
+        customPropThief.Add("Team", "Thief");
+
+        if(PhotonNetwork.InRoom) // Check if connected to a room
+        {
+            int i = 0;
+            foreach (Player player in PhotonNetwork.PlayerList)
+            {
+                if (i == rand)
+                {
+                    player.SetCustomProperties(customPropCop);
+                }
+                else
+                {
+                    player.SetCustomProperties(customPropThief);
+                }
+                i++;
+            }
+        }
+
     }
 }
--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class SpawnPlayers : MonoBehaviour
 {
-    public GameObject playerPrefab;
+    public GameObject theifPrefab;
+    public GameObject copPrefab;
 
     public float minX;
     public float maxX;
@@ -14,7 +17,24 @@ public class SpawnPlayers : MonoBehaviour
 
     private void Start()
     {
+        Hashtable cop = new Hashtable();
+        Hashtable thief = new Hashtable();
+
+        cop.Add("Team", "Cop");
+        thief.Add("Team", "Thief");
+
         Vector3 randomPos = new Vector3(Random.Range(minX, maxX), yPos, Random.Range(minZ, maxZ));
-        PhotonNetwork.Instantiate(playerPrefab.name, randomPos, Quaternion.identity);
+        foreach(Player player in PhotonNetwork.PlayerList)
+        {
+            if(player.CustomProperties == thief)
+            {
+                PhotonNetwork.Instantiate(theifPrefab.name, randomPos, Quaternion.identity);
+            }
+            else if (player.CustomProperties == cop)
+            {
+                PhotonNetwork.Instantiate(copPrefab.name, randomPos, Quaternion.identity);
+            }
+            
+        }
     }
 }
