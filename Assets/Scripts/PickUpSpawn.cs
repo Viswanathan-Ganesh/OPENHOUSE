5e0a0cd9becf2b83114855a042af629c116fe2e6
Suriyaa-Prasath <159170300+Suriyaa-Prasath@users.noreply.github.com>
2024-03-03 04:33:29 +0530

    Project FIles

--- Assets/Scripts/PickUpSpawn.cs
+++ Assets/Scripts/PickUpSpawn.cs
@@ -5,16 +5,10 @@ public class PickUpSpawn : MonoBehaviour
 {
     public GameObject pickUp;
 
-
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.Instantiate(pickUp.name,  transform.position, Quaternion.identity);
+        PhotonNetwork.Instantiate(pickUp.name, transform.position, Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
